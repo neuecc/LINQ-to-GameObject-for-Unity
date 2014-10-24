3b9819a965fde617efa62b5039bac1b6d6448b46
neuecc <ils@neue.cc>
2014-10-24 23:56:48 +0900

    add Descendants test

--- Assets/Editor/Tests/TraverseTest.cs
+++ Assets/Editor/Tests/TraverseTest.cs
@@ -29,5 +29,12 @@ namespace UnityTest
                 .IsCollection("Sphere_A", "Sphere_B", "Group", "Sphere_A", "Sphere_B");
         }
 
+
+        [Test]
+        public void Descendants()
+        {
+            Origin.Descendants().Select(x => x.name)
+                .IsCollection("Sphere_A", "Sphere_B", "Group", "P1", "P2", "P3", "P4", "Sphere_A", "Sphere_B");
+        }
     }
 }
