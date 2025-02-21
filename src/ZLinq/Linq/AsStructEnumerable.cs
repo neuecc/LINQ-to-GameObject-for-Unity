68439fa82b1fb241594ee9382ea3c1ab62963fe8
neuecc <ils@neue.cc>
2025-02-21 11:23:44 +0900

    range

--- src/ZLinq/Linq/AsStructEnumerable.cs
+++ src/ZLinq/Linq/AsStructEnumerable.cs
@@ -17,9 +17,24 @@
             return new(source);
         }
 
+        public static MemoryStructEnumerable<T> AsStructEnumerable<T>(this Memory<T> source)
+        {
+            return new(source);
+        }
+
+        public static MemoryStructEnumerable<T> AsStructEnumerable<T>(this ReadOnlyMemory<T> source)
+        {
+            return new(source);
+        }
+
 #if NET9_0_OR_GREATER
 
         public static SpanStructEnumerable<T> AsStructEnumerable<T>(this Span<T> source)
+        {
+            return new(source);
+        }
+
+        public static SpanStructEnumerable<T> AsStructEnumerable<T>(this ReadOnlySpan<T> source)
         {
             return new(source);
         }
@@ -109,6 +124,43 @@ namespace ZLinq.Linq
 
     [StructLayout(LayoutKind.Auto)]
     [EditorBrowsable(EditorBrowsableState.Never)]
+    public ref struct MemoryStructEnumerable<T>(ReadOnlyMemory<T> source) : IStructEnumerable<T>
+    {
+#if NET9_0_OR_GREATER
+        ReadOnlySpan<T> source = source.Span;
+#endif
+
+        int index;
+
+        public bool TryGetNonEnumeratedCount(out int count)
+        {
+            count = source.Length;
+            return false;
+        }
+
+        public bool TryGetNext(out T current)
+        {
+            if (index < source.Length)
+            {
+#if NET9_0_OR_GREATER
+                current = source[index++];
+#else
+                current = source.Span[index++];
+#endif
+                return true;
+            }
+
+            Unsafe.SkipInit(out current);
+            return false;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    [StructLayout(LayoutKind.Auto)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public struct ListStructEnumerable<T>(List<T> source) : IStructEnumerable<T>
     {
         bool isInit = false;
@@ -151,9 +203,9 @@ namespace ZLinq.Linq
 
     [StructLayout(LayoutKind.Auto)]
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public ref struct SpanStructEnumerable<T>(Span<T> source) : IStructEnumerable<T>
+    public ref struct SpanStructEnumerable<T>(ReadOnlySpan<T> source) : IStructEnumerable<T>
     {
-        Span<T> source = source;
+        ReadOnlySpan<T> source = source;
         int index;
 
         public bool TryGetNonEnumeratedCount(out int count)
---
+++ src/ZLinq/Linq/Range.cs
@@ -0,0 +1,46 @@
+﻿namespace ZLinq
+{
+    public static partial class StructEnumerable
+    {
+        public static RangeStructEnumerable<T> Range<T>(int start, int count)
+        {
+            // TODO: varidation
+            return new(start, count);
+        }
+    }
+}
+
+namespace ZLinq.Linq
+{
+    [StructLayout(LayoutKind.Auto)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public struct RangeStructEnumerable<T>(int start, int count) : IStructEnumerable<int>
+    {
+        // TODO: impl
+        readonly int count = count;
+        int start = start;
+        int to = start + count;
+
+        public bool TryGetNonEnumeratedCount(out int count)
+        {
+            count = this.count;
+            return true;
+        }
+
+        public bool TryGetNext(out int current)
+        {
+            if (start < to)
+            {
+                current = start++;
+                return true;
+            }
+
+            current = 0;
+            return false;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
