989e38ea56b78d07cfa9f91b46cc1d06d5f5c753
neuecc <ils@neue.cc>
2025-02-26 21:42:17 +0900

    Range SIMD

--- src/ZLinq/ValueEnumerable.RangeRepeatEmpty.cs
+++ src/ZLinq/ValueEnumerable.RangeRepeatEmpty.cs
@@ -1,4 +1,6 @@
-﻿namespace ZLinq
+﻿using System.Numerics;
+
+namespace ZLinq
 {
     public static partial class ValueEnumerable
     {
@@ -6,7 +8,12 @@
 
         public static RangeValueEnumerable<T> Range<T>(int start, int count)
         {
-            // TODO: varidation
+            long max = ((long)start) + count - 1;
+            if (count < 0 || max > int.MaxValue)
+            {
+                Throws.ArgumentOutOfRangeException(nameof(count));
+            }
+
             return new(start, count);
         }
     }
@@ -50,5 +57,59 @@ namespace ZLinq.Linq
         public void Dispose()
         {
         }
+
+
+        public T[] ToArray()
+        {
+            // TODO: SIMD
+            throw new NotImplementedException();
+        }
+
+
+#if NET8_0_OR_GREATER
+
+        // borrowed from .NET Enumerable.Range vectorized fill, originally implemented by @neon-sunset.
+        static void FillIncremental(Span<int> span, int from)
+        {
+            ref var pointer = ref MemoryMarshal.GetReference(span);
+            ref var end = ref Unsafe.Add(ref pointer, span.Length);
+
+            if (Vector.IsHardwareAccelerated
+#if NET8_0
+                && Vector<int>.Count <= 16
+#endif
+                && span.Length >= Vector<int>.Count)
+            {
+                // for example, from = 5
+#if NET9_0_OR_GREATER
+                var indices = Vector<int>.Indices;                  // <0, 1, 2, 3, 4, 5, 6, 7>...
+#else
+                var indices = new Vector<int>((ReadOnlySpan<int>)new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+#endif
+                var data = indices + new Vector<int>(from);         // <5, 6, 7, 8, 9, 10, 11, 12>...
+                var increment = new Vector<int>(Vector<int>.Count); // <8, 8, 8, 8, 8, 8, 8, 8>...
+
+                ref var to = ref Unsafe.Subtract(ref end, Vector<int>.Count);
+                do
+                {
+                    data.StoreUnsafe(ref pointer);                              // copy vectorized data to Span pointer
+                    data += increment;                                          // <13, 14, 15, 16, 17, 18, 19, 20>...
+                    pointer = ref Unsafe.Add(ref pointer, Vector<int>.Count);   // move pointer++
+
+                } while (Unsafe.IsAddressLessThan(ref pointer, ref to)); // available space for vectorized copy
+                // TODO: !Unsafe.IsAddressGreaterThan(ref to, ref pointer)
+
+                from = data[0]; // next value for fill
+            }
+
+            // fill rest
+            while (Unsafe.IsAddressLessThan(ref pointer, ref end))
+            {
+                pointer = from++; // reusing from
+                pointer = ref Unsafe.Add(ref pointer, 1);
+            }
+        }
+
+#endif
     }
 }
---
+++ tests/ZLinq.Tests/Linq/RangeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLinq.Tests.Linq;
+
+
+public class RangeTest
+{
+    [Fact]
+    public void Foo()
+    {
+        throw new NotImplementedException();
+    }
+}
