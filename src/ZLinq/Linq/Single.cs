76bdf8f3c345f4f325c918b3825dfa9ae746e037
neuecc <ils@neue.cc>
2025-03-18 17:48:04 +0900

    s

--- src/ZLinq/Linq/Single.cs
+++ src/ZLinq/Linq/Single.cs
@@ -20,5 +20,40 @@
             throw new NotImplementedException();
         }
 
+        public static TSource SingleOrDefault<TEnumerator, TSource>(in this ValueEnumerable<TEnumerator, TSource> source)
+    where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+    , allows ref struct
+#endif
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TSource SingleOrDefault<TEnumerator, TSource>(in this ValueEnumerable<TEnumerator, TSource> source, TSource defaultValue)
+            where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+            , allows ref struct
+#endif
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TSource SingleOrDefault<TEnumerator, TSource>(in this ValueEnumerable<TEnumerator, TSource> source, Func<TSource, Boolean> predicate)
+            where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+            , allows ref struct
+#endif
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TSource SingleOrDefault<TEnumerator, TSource>(in this ValueEnumerable<TEnumerator, TSource> source, Func<TSource, Boolean> predicate, TSource defaultValue)
+            where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+            , allows ref struct
+#endif
+        {
+            throw new NotImplementedException();
+        }
     }
 }
